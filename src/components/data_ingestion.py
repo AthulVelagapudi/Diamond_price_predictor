22d6343bcf0d367cfed40f166283d36720634086
VelagapudiAthul <velagapudiathul@gmail.com>
2023-08-16 15:26:34 +0200

    Training pipeline

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -31,6 +31,14 @@ class DataIngestion:
             train_set, test_set = train_test_split(df,test_size=.3, random_state = 42)
             train_set.to_csv(self.ingestion_config.train_data_path, index = False, header = True)
             test_set.to_csv(self.ingestion_config.test_data_path, index = False, header = True)
+
+            logging.info('Ingestion of Data is completed')
+            
+            return(
+                self.ingestion_config.train_data_path,
+                self.ingestion_config.test_data_path
+
+            )
         
         except Exception as e:
             logging.info('Exception occured at Data Ingestion stage')
